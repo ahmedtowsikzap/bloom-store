80fc2e58ecf6da4c89c136ae12216a7e879492d5
ahmedtowsikzap <ahmedtowsikzap@gmail.com>
2022-07-01 22:18:30 +0600

    created announcement section design

--- src/components/Announcement.jsx
+++ src/components/Announcement.jsx
@@ -1,1 +1,12 @@
+import styled from "styled-components";
 
+const Container = styled.div`
+  height: 30px;
+  background-color: teal;
+  color: white;
+`;
+const Announcement = () => {
+  return <Container>save 50% of starting from 25 july</Container>;
+};
+
+export default Announcement;
