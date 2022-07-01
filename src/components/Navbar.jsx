6b5dc3c50090fbcad6b75f03c025c91435465bf7
ahmedtowsikzap <ahmedtowsikzap@gmail.com>
2022-07-01 22:17:18 +0600

    completed navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,6 @@
 import { Search } from "@mui/icons-material";
+import ShoppingCartOutlinedIcon from "@mui/icons-material/ShoppingCartOutlined";
+import { Badge } from "@mui/material";
 import styled from "styled-components";
 
 const Container = styled.div`
@@ -34,6 +36,7 @@ const Input = styled.input`
 `;
 const Center = styled.div`
   flex: 1;
+  text-align: center;
 `;
 
 const Logo = styled.h1`
@@ -41,6 +44,14 @@ const Logo = styled.h1`
 `;
 const Right = styled.div`
   flex: 1;
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+`;
+const MenuItem = styled.div`
+  font-size: 14px;
+  cursor: pointer;
+  margin-left: 25px;
 `;
 
 const Navbar = () => {
@@ -48,16 +59,24 @@ const Navbar = () => {
     <Container>
       <Wrapper>
         <Left>
-          <Language>ENG</Language>
+          <Language>ENGLISH</Language>
           <SearchContainer>
             <Input />
-            <Search />
+            <Search style={{ color: "gray", fontSize: 16 }} />
           </SearchContainer>
         </Left>
         <Center>
-          <Logo>BLOOM</Logo>
+          <Logo>BLOOM.</Logo>
         </Center>
-        <Right>Right</Right>
+        <Right>
+          <MenuItem>REGISTER</MenuItem>
+          <MenuItem>SIGN IN</MenuItem>
+          <MenuItem>
+            <Badge badgeContent={3} color="success">
+              <ShoppingCartOutlinedIcon />
+            </Badge>
+          </MenuItem>
+        </Right>
       </Wrapper>
     </Container>
   );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,10 @@
+import Announcement from "../components/Announcement";
 import Navbar from "../components/Navbar";
 
 const Home = () => {
   return (
     <div>
+      <Announcement />
       <Navbar />
     </div>
   );
