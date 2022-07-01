17e131a2533f13edce49637d59621b3b070c5cb6
ahmedtowsikzap <ahmedtowsikzap@gmail.com>
2022-07-01 21:46:29 +0600

    material UI & icon themes added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,3 +1,4 @@
+import { Search } from "@mui/icons-material";
 import styled from "styled-components";
 
 const Container = styled.div`
@@ -7,25 +8,55 @@ const Container = styled.div`
 const Wrapper = styled.div`
   padding: 10px 20px;
   display: flex;
+  align-items: center;
   justify-content: space-between;
 `;
 
 const Left = styled.div`
   flex: 1;
+  display: flex;
+  align-items: center;
+`;
+const Language = styled.span`
+  font-size: 14px;
+  cursor: pointer;
+`;
+
+const SearchContainer = styled.div`
+  border: 0.5px solid lightgray;
+  display: flex;
+  align-items: center;
+  margin-left: 25px;
+  padding: 5px;
+`;
+const Input = styled.input`
+  border: none;
 `;
 const Center = styled.div`
-  width: 33.3%;
+  flex: 1;
+`;
+
+const Logo = styled.h1`
+  font-weight: bold;
 `;
 const Right = styled.div`
-  width: 33.3%;
+  flex: 1;
 `;
 
 const Navbar = () => {
   return (
     <Container>
       <Wrapper>
-        <Left>lorem</Left>
-        <Center>Center</Center>
+        <Left>
+          <Language>ENG</Language>
+          <SearchContainer>
+            <Input />
+            <Search />
+          </SearchContainer>
+        </Left>
+        <Center>
+          <Logo>BLOOM</Logo>
+        </Center>
         <Right>Right</Right>
       </Wrapper>
     </Container>
