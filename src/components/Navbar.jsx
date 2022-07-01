a4056f8c59909a86a4c71b56dc5025ba49bc3c2e
ahmedtowsikzap <ahmedtowsikzap@gmail.com>
2022-07-01 21:19:50 +0600

    created left right and center for navbar size with flex

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,10 +2,34 @@ import styled from "styled-components";
 
 const Container = styled.div`
   height: 60px;
-  background-color: #de8604;
 `;
+
+const Wrapper = styled.div`
+  padding: 10px 20px;
+  display: flex;
+  justify-content: space-between;
+`;
+
+const Left = styled.div`
+  flex: 1;
+`;
+const Center = styled.div`
+  width: 33.3%;
+`;
+const Right = styled.div`
+  width: 33.3%;
+`;
+
 const Navbar = () => {
-  return <Container>Navbar</Container>;
+  return (
+    <Container>
+      <Wrapper>
+        <Left>lorem</Left>
+        <Center>Center</Center>
+        <Right>Right</Right>
+      </Wrapper>
+    </Container>
+  );
 };
 
 export default Navbar;
