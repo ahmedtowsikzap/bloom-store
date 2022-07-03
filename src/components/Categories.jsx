22baad838ff6ac1b07d8910ea994595a51993658
ahmedtowsikzap <ahmedtowsikzap@gmail.com>
2022-07-03 17:05:37 +0600

    categories section & folder structure through mapping

---
+++ src/components/Categories.jsx
@@ -0,0 +1,19 @@
+import styled from "styled-components"
+import { categories } from "../data"
+import CategoryItem from "./CategoryItem"
+const Container = styled.div`
+    
+
+`
+const Categories = () => {
+  return (
+    <Container>
+       {categories.map(item=>(
+        <CategoryItem item={item}/>
+       ))}
+
+    </Container>
+  )
+}
+
+export default Categories
---
+++ src/components/CategoryItem.jsx
@@ -0,0 +1,14 @@
+import styled from "styled-components"
+
+const Container = styled.div`
+    display: flex;
+    padding: 20px;
+
+`
+const CategoryItem = ({item}) => {
+  return (
+    <Container>CategoryItem</Container>
+  )
+}
+
+export default CategoryItem
