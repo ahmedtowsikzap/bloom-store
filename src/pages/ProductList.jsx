c21aa1a7e51e5e83800bac2d4637b87ba6a0c2a3
ahmedtowsikzap <ahmedtowsikzap@gmail.com>
2022-07-06 01:22:45 +0600

    filter section & newsletter & footer addded

--- src/pages/ProductList.jsx
+++ src/pages/ProductList.jsx
@@ -1,15 +1,36 @@
 import styled from "styled-components"
 import Announcement from "../components/Announcement"
+import Footer from "../components/Footer"
 import Navbar from "../components/Navbar"
+import Newsletter from "../components/Newsletter"
+import Products from "../components/Products"
 
 const Container  = styled.div`
     
+`
+const Title  = styled.div`
+    margin: 20px;
+`
+const FilterContainer  = styled.div`
+    display: flex;
+    justify-content: space-between;
+`
+const Filter  = styled.div`
+    margin: 20px;
 `
 const ProductList = () => {
   return (
     <Container>
         <Navbar/>
         <Announcement/>
+        <Title>Dresses</Title>
+        <FilterContainer>
+          <Filter>filter</Filter>
+          <Filter>filter2</Filter>
+        </FilterContainer>
+        <Products/>
+        <Newsletter/>
+        <Footer/>
     </Container>
   )
 }
