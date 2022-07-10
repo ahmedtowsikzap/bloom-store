4b5252a19279f3dc0cdcabfe914b026d3503b843
ahmedtowsikzap <ahmedtowsikzap@gmail.com>
2022-07-10 22:32:54 +0600

    changed route path and added navbar to cart

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,12 +1,12 @@
-import styled from "styled-components"
-
+import styled from "styled-components";
+import Navbar from "../components/Navbar";
 const Container = styled.div`
     
 `
 const Cart = () => {
   return (
     <Container>
-
+       <Navbar/>
         
     </Container>
   )
