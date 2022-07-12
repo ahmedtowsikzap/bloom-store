ea1eb4a10f36c87427e00fe97b77785578eff54e
ahmedtowsikzap <ahmedtowsikzap@gmail.com>
2022-07-13 01:59:24 +0600

    cart section total price or subtotal

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -50,6 +50,10 @@ const Info = styled.div`
 `
 const Summary = styled.div`
   flex: 1;
+  border: 0.5px solid lightgray;
+  border-radius: 10px;
+  padding: 20px;
+  height: 50vh;
   
 `
 const Details = styled.div`
@@ -107,6 +111,26 @@ const ProductAmount = styled.div`
 const Productprice= styled.div`
   font-size: 27px;
   font-weight: 500;
+`
+const SummaryTitle = styled.h1`
+  font-weight:200;
+
+`
+const SummaryItem = styled.div`
+  margin: 30px 0px;
+  display: flex;
+  justify-content: space-between;
+  font-weight: ${props=>props.type === "total" && "500"};
+  font-size: ${props=>props.type === "total" && "24px"};
+`
+const SummaryItemText = styled.span`
+  
+`
+const SummaryItemPrice = styled.span`
+  
+`
+const Button = styled.button`
+  
 `
 
 
@@ -182,8 +206,8 @@ const Cart = () => {
                   <SummaryItemText>Shipping Discount</SummaryItemText>
                   <SummaryItemPrice>$ -7.90</SummaryItemPrice>
                 </SummaryItem>
-                <SummaryItem>
-                  <SummaryItemText type="total">Total</SummaryItemText>
+                <SummaryItem type="total">
+                  <SummaryItemText>Total</SummaryItemText>
                   <SummaryItemPrice>$ 109</SummaryItemPrice>
                 </SummaryItem>
                 <Button>Checkout Now!</Button>
