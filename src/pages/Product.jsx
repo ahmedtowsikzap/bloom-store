ae8274aa6b964bce8a0077900a02488f80db9100
ahmedtowsikzap <ahmedtowsikzap@gmail.com>
2022-07-13 22:28:53 +0600

    Single product section responsive image & color selection

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -3,12 +3,14 @@ import styled from "styled-components"
 import Announcement from "../components/Announcement"
 import Footer from "../components/Footer"
 import Navbar from "../components/Navbar"
+import { mobile } from "../responsive"
 const Container = styled.div`
     
 `
 const Wrapper = styled.div`
     padding: 50px;
     display: flex;
+    ${mobile({padding:"10px", flexDirection: "column"})}
     
 `
 const ImageContainer = styled.div`
@@ -18,10 +20,12 @@ const Image = styled.img`
     width: 100%;
     height: 90vh;
     object-fit: cover;
+    ${mobile({height: "40vh"})}
 `
 const InfoContainer = styled.div`
   flex: 1;
   padding: 0px 50px;
+  ${mobile({padding: "10px"})}
 `
 const Title = styled.h1`
     font-weight: 300;
