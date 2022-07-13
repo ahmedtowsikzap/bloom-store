78013b66637b22571eee955a3687f46285ab76d9
ahmedtowsikzap <ahmedtowsikzap@gmail.com>
2022-07-13 22:24:40 +0600

    changed route options

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
-import ProductList from "./pages/ProductList";
+import Product from "./pages/Product";
 const App = () => {
-  return <ProductList />;
+  return <Product />;
 };
 export default App;
