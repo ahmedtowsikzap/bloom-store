e17cfd5d5d0655eb6d8992dbf3248bbd3039ebb3
ahmedtowsikzap <ahmedtowsikzap@gmail.com>
2022-07-24 01:32:13 +0600

    setup routing path for for cart , login, register & porudcts

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,36 @@
+import {
+  BrowserRouter as Router, Route, Switch
+} from "react-router-dom";
 import Home from "../src//pages/Home";
+import Cart from "./pages/Cart";
+import Login from "./pages/Login";
+import Product from "./pages/Product";
+import ProductList from "./pages/ProductList";
+import Register from "./pages/Register";
 const App = () => {
-  return <Home />;
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/products/:category">
+          <ProductList/>
+        </Route>
+        <Route path="/product/:id">
+          <Product />
+        </Route>
+        <Route path="/cart">
+          <Cart />
+        </Route>
+        <Route path="/login">
+          <Login />
+        </Route>
+        <Route path="/register">
+          <Register />
+        </Route>
+      </Switch>
+    </Router>
+  );
 };
 export default App;
